e24b43ff76a97a3fbb6b98f2cc54173d1c621399
Marina Capelo <marina.gac30@gmail.com>
2025-03-12 16:53:50 -0300

    Initial Commit

---
+++ entertainmentApp/entertainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  entertainmentAppApp.swift
+//  entertainmentApp
+//
+//  Created by found on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct entertainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ entertainmentAppTests/entertainmentAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  entertainmentAppTests.swift
+//  entertainmentAppTests
+//
+//  Created by found on 12/03/25.
+//
+
+import Testing
+@testable import entertainmentApp
+
+struct entertainmentAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
